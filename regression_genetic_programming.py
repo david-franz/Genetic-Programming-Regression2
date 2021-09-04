9429b9fb2553a2aedddc29696faa09af0bd5ec45
David Franz <davidfranznz@gmail.com>
2021-09-05 02:03:54 +1200

    working code. code adapted very easily from previous genetic programming regression example despite being a considerably more difficult function this time.

--- regression_genetic_programming.py
+++ regression_genetic_programming.py
@@ -1,54 +1,53 @@
-# Note: for part 2, my code was adapted from the code discussed in these tutorials:
-# https://deap.readthedocs.io/en/master/examples/gp_symbreg.html 
-# and 
-# https://deap.readthedocs.io/en/master/tutorials/advanced/gp.html
-
-from deap import gp, creator, base, tools, algorithms
-import numpy as np
-import operator
+import sys
 import math
 import random
+import operator
+
+import numpy as np
+from deap import gp, creator, base, tools, algorithms
 
 print("\nNote that this takes a while to run with the current parameters (~30 seonds on my computer). If you want a faster run, feel free to drop the population_size parameter at the top of the code. This will probably give less accurate functions (with higher fitness).")
 
-depth_of_tree = 4
-population_size = 15000
+depth_of_tree = 7
+population_size = 27000
 
-# given values of original function
-mapping_dictionary = {	-2.00 : 37.00000,
-						-1.75 : 24.16016,
-						-1.50 : 15.06250,
-						-1.25 : 8.91016,
-						-1.00 : 5.00000,
-						-0.75 : 2.72266,
-						-0.50 : 1.56250,
-						-0.25 : 1.09766,
-						 0.00 : 1.00000,
-						 0.25 : 1.03516,
-						 0.50 : 1.06250,
-						 0.75 : 1.03516, 
-						 1.00 : 1.00000,
-						 1.25 : 1.09766,
-						 1.50 : 1.56250,
-						 1.75 : 2.72266,
-						 2.00 : 5.00000,
-						 2.25 : 8.91016,
-						 2.50 : 15.06250,
-						 2.75 : 24.16016	}
+def function_to_find(x):
+	if x <= 0:
+		return (2*x)+(x*x)+3.0
+	else:
+		return (1/x) + math.sin(x) # might need to change to protected division
 
 # generates all the x values we need to assess our function at
-float_range_array = np.arange(-2.0, 3.0, 0.25)
+float_range_array = np.arange(-5.0, 5.0, 0.25)
 float_range_list = list(float_range_array)
+
+mapping_dictionary = dict()
+for x in float_range_array:
+	mapping_dictionary[x] = function_to_find(x)
+
+def if_function(x, constant, individual1, individual2):
+	if x <= constant:
+		return individual1
+	else:
+		return individual2
+
+def protected_division(left, right):
+	if right == 0:
+		return 1
+	return left / right
 
 # fitness function:
 # sum of squared area of function vs given points
-def evalSymbReg(individual):
+def evaluate_symbol_regression(individual):
 	# Transform the tree expression in a callable function
 	func = toolbox.compile(expr=individual)
 
+	#print(individual)
 	# Evaluate the mean squared error between the created function and the given function points
 	squared_error = 0
 	for x in float_range_array:
+		if ("sin(protected_division" in str(individual)) and x == 0.0:
+			continue
 		mapping_dictionary[x]
 		squared_error += (func(x) - mapping_dictionary[x])**2
 	
@@ -60,6 +59,10 @@ if __name__ == '__main__':
 	pset.addPrimitive(operator.sub, 2)
 	pset.addPrimitive(operator.mul, 2)
 	pset.addPrimitive(operator.neg, 1)
+	pset.addPrimitive(protected_division, 2)
+	pset.addPrimitive(if_function, 4)
+	pset.addPrimitive(math.sin, 1)
+	pset.addPrimitive(math.cos, 1)
 	pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
 	pset.renameArguments(ARG0="x")
 	pset.renameArguments(ARG1="y")
@@ -73,7 +76,7 @@ if __name__ == '__main__':
 	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 	toolbox.register("compile", gp.compile, pset=pset)
 
-	toolbox.register("evaluate", evalSymbReg)
+	toolbox.register("evaluate", evaluate_symbol_regression)
 	toolbox.register("select", tools.selTournament, tournsize=3)
 	toolbox.register("mate", gp.cxOnePoint)
 	toolbox.register("expr_mut", gp.genFull, min_=0, max_=3)
@@ -97,5 +100,5 @@ if __name__ == '__main__':
 	
 	print("\n------------------------------------------------------------")
 	print("function = {}\n".format(hof[0]))
-	print("fitness = {}".format(evalSymbReg(hof[0])))
+	print("fitness = {}".format(evaluate_symbol_regression(hof[0])))
 	print("------------------------------------------------------------\n")
